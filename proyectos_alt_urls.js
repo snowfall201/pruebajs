2014d09cf6a186226862eeb6813c69056f6009fb
Alejandro León <145546896+Alejandroleon2002@users.noreply.github.com>
2025-06-11 19:04:52 +0200

    Create proyectos_alt_urls.js

---
+++ proyectos_alt_urls.js
@@ -0,0 +1,41 @@
+// -----------------------------------------------------------------------------
+// Qué tengo que hacer yo:
+// -----------------------------------------------------------------------------
+// - Cuando añada o cambie proyectos en data.json (ya sea a mano o por scraping),
+//   tengo que ejecutar: node generar_alt_urls.js
+// - El script meterá automáticamente aquí las claves de los nuevos proyectos.
+// - Si quiero poner una URL alternativa para algún proyecto, solo cambio el valor de la clave.
+//   (No borro ni cambio las claves que ya hay.)
+// - Si ya he puesto una URL alternativa, se mantiene aunque añada nuevos proyectos.
+// -----------------------------------------------------------------------------
+window.PROYECTOS_ALT_URLS = {
+  "/proyectos/683325/detalle": "",
+  "/proyectos/196762/detalle": "",
+  "/proyectos/1094307/detalle": "",
+  "/proyectos/340384/detalle": "",
+  "/proyectos/1094333/detalle": "",
+  "/proyectos/1094356/detalle": "",
+  "/proyectos/196148/detalle": "",
+  "/proyectos/1094362/detalle": "",
+  "/proyectos/196163/detalle": "",
+  "/proyectos/1094396/detalle": "",
+  "/proyectos/196092/detalle": "",
+  "/proyectos/196016/detalle": "",
+  "/proyectos/196723/detalle": "",
+  "/proyectos/196077/detalle": "",
+  "/proyectos/195992/detalle": "",
+  "/proyectos/196578/detalle": "",
+  "/proyectos/196522/detalle": "",
+  "/proyectos/1094280/detalle": "",
+  "/proyectos/1094346/detalle": "",
+  "/proyectos/196870/detalle": "",
+  "/proyectos/1094372/detalle": "",
+  "/proyectos/196370/detalle": "",
+  "/proyectos/196445/detalle": "",
+  "/proyectos/196446/detalle": "",
+  "/proyectos/1094279/detalle": "",
+  "/proyectos/196275/detalle": "",
+  "/proyectos/196691/detalle": "",
+  "/proyectos/196805/detalle": "",
+  "/proyectos/196780/detalle": "",
+};
